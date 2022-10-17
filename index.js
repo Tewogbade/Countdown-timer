e2b023e0723a62581312781a4afe54ee241a93f0
Tewogbade <tewogbade3008@gmail.com>
2022-10-17 20:53:26 +0100

    Added what should happen when the time is over

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 "use strict";
-let countDownTime = 14 * 24 * 60 * 60;
-const second = 1;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
+const countDate = new Date("Oct 30 2022 12:00:00").getTime();
 
-const countdown = () => {
+const countDown = () => {
+  const now = new Date().getTime();
+  const countDownTime = countDate - now;
+  if (countDownTime <= 1000) {
+    document.querySelector(".text-time").style.display = "none";
+    document.querySelector(".expired").textContent = "TIME HAS EXPIRED!";
+  }
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
   let textDay = Math.floor(countDownTime / day);
   let textHour = Math.floor((countDownTime % day) / hour);
   let textMinute = Math.floor((countDownTime % hour) / minute);
   let textSecond = Math.floor((countDownTime % minute) / second);
 
-  document.querySelector(".day").textContent = textDay =
+  document.querySelector(".day").textContent =
     textDay < 10 ? `0${textDay}` : `${textDay}`;
-  document.querySelector(".hour").textContent = textHour =
+  document.querySelector(".hour").textContent =
     textHour < 10 ? `0${textHour}` : `${textHour}`;
-  document.querySelector(".minute").textContent = textMinute =
+  document.querySelector(".minute").textContent =
     textMinute < 10 ? `0${textMinute}` : `${textMinute}`;
   document.querySelector(".second").textContent =
     textSecond < 10 ? `0${textSecond}` : `${textSecond}`;
-  countDownTime--;
+  console.log(countDownTime);
 };
-setInterval(countdown, 1000);
+setInterval(countDown, 1000);
